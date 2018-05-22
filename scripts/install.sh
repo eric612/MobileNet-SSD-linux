31b8da155df1e3a4b4bb62319bea2da3a85ffa5a
Ming-Yang Liu <eric612kimo@yahoo.com.tw>
2018-05-22 15:04:20 +0800

    Initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+# install opencv dependency
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+
+
+# install caffe dependency
+sudo apt-get install libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libyaml-dev
+sudo apt-get install python-numpy python-setuptools python-pip cython python-skimage python-protobuf
+sudo pip install easydict PyYAML
+
