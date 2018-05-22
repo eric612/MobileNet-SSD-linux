df6c37a9877b4d007877c09379298e437801dd16
Ming-Yang Liu <eric612kimo@yahoo.com.tw>
2018-05-22 15:11:22 +0800

    Initial commit

--- demo_video.sh
+++ demo_video.sh
@@ -1,1 +1,1 @@
-./examples/ssd/ssd_detect models/MobileNetV2/deploy_voc_retrain_deploy.prototxt models/MobileNetV2/MobileNetSSD_deploy_iter_3000.caffemodel -file_type video -mean_value 0.5,0.5,0.5 -normalize_value 0.007843 -confidence_threshold 0.3
+./examples/ssd/ssd_detect models/MobileNetV2/deploy_voc_retrain_deploy.prototxt models/MobileNetV2/MobileNetSSD_deploy_iter_27000.caffemodel -file_type video -mean_value 0.5,0.5,0.5 -normalize_value 0.007843 -confidence_threshold 0.3
